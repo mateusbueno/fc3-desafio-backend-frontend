4c130c85281bb50e796471115af1277fc9627d2f
Mateus Bueno <mateusob@gmail.com>
2021-07-07 21:02:45 -0300

    test

---
+++ frontend2/src/model.ts
@@ -0,0 +1,11 @@
+export interface Route {
+    title: string;
+    startPosition: {
+        latitude: string,
+        longitude: string
+    };
+    endPosition: {
+        latitude: string,
+        longitude: string
+    };
+}
